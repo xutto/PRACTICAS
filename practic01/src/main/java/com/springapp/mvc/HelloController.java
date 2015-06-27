c2e941ae1a67606d19072dd8c15d270712fe1e7d
xutto <neo_xutto@hotmail.com>
2015-06-27 18:41:47 +0200

    ded

--- practic01/src/main/java/com/springapp/mvc/HelloController.java
+++ practic01/src/main/java/com/springapp/mvc/HelloController.java
@@ -11,6 +11,7 @@ public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 
+
         /*asdasdasd*/
 		model.addAttribute("message", "Hello world!");
 		return "hello";
