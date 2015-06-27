04ca7ce61daa62a293e84190eb485b3fbcacbbb8
xutto <neo_xutto@hotmail.com>
2015-06-27 19:26:03 +0200

    zcsdf

--- practic01/src/main/java/com/springapp/mvc/HelloController.java
+++ practic01/src/main/java/com/springapp/mvc/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
 	public String printWelcome(ModelMap model) {
 
 
-        /*asdasdas sdfsfsf  dfhdhdh asasadasdsdd*/
+        /*asdasdas sdfsfsf  dfhdhdh asasadadfsdfsdfsfsfsfsfsdsdd*/
 		model.addAttribute("message", "Hello world!");
 		return "hello";
 	}
